26e3ac2e102e7e4f7651aae14fe635a0f6fb752c
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-03 17:45:39 -0400

    add poros functionality

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -3,21 +3,21 @@ module Api
     class TeamsController < ApplicationController
       def index
         teams = TeamsFacade.advanced_stat_search(params[:year])
-        search_criterion(teams)
+        # top_15 = search_criterion(teams)
+        render json: TeamsSerializer.new(teams)
       end
 
-    private
-      def search_criterion(teams)
-        if !params[:overall].nil?
-          top_15 = teams.sort_by{|team|team.total_havoc}.reverse!.slice!(0..24)
-        elsif !params[:defensive_line].nil?
-          #top_15_defensive_line
-        elsif !params[:db].nil?
-          #top_15_dbs
-        else
-          #error
-        end
-      end
+    # private
+    #   def search_criterion(teams)
+    #     if !params[:overall].nil?
+    #       teams.sort_by{|team|team.total_havoc}.reverse!.slice!(0..14)
+    #     elsif !params[:dl].nil?
+    #       teams.sort_by{|team|team.defensive_line_havoc}.reverse!.slice!(0..14)
+    #     elsif !params[:db].nil?
+    #       teams.sort_by{|team|team.db_havoc}.reverse!.slice!(0..14)
+    #     else
+    #       #error
+    #     end
     end
   end
 end
--- app/poros/team_defense.rb
+++ app/poros/team_defense.rb
@@ -1,9 +1,11 @@
 class TeamDefense
-  attr_reader :name, :total_havoc
+  attr_reader :name, :total_havoc, :defensive_line_havoc, :db_havoc
 
   def initialize(data)
     @name = data[:team]
     @total_havoc = data[:defense][:havoc][:total]
+    @defensive_line_havoc = data[:defense][:havoc][:frontSeven]
+    @db_havoc = data[:defense][:havoc][:db]
   end
   
 end
