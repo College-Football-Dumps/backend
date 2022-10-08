444c45d7de541ae28d06eb5db20fd7dae2d7b6a5
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-08 14:33:34 -0400

    add shoulda matchers to gemfile

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,11 +63,18 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
- VCR.configure do |config|
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data('<football_key>') { ENV['football_key'] }
   config.configure_rspec_metadata!
-  end
+end
 
 
