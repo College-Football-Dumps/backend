f8b2d24406c884469fa7ae5470571eb52cf109cc
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-08 16:45:54 -0400

    fix lack of json formatting, add proper json gems

--- spec/requests/api/v1/team_request_spec.rb
+++ spec/requests/api/v1/team_request_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Team Data' do
   it 'returns team advanced stats information, happy path', :vcr do
     get '/api/v1/teams', headers: headers, params: { year: "2021"  }
-
     expect(response).to be_successful
     expect(response.status).to eq(200)
-
+    
+    require 'pry'; binding.pry 
     teams_defense = JSON.parse(response.body, symbolize_names: true)
     
     expect(teams_defense).to be_a(Hash)
